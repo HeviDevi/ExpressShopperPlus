6c4939dea924da1a26d7a6a0e58d3aad5a4339c8
HeviDevi <devon.r.ross@gmail.com>
2024-06-16 11:41:09 -0400

    file structure change

---
+++ Develop/models/Category.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+   id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+   },
+    category_name: {
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
